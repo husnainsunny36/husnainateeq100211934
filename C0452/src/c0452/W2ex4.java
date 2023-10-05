44c20619e0417e09a7a3aee077d71c7b8264e853
husna <husna@Sunny>
2023-10-05 20:51:14 +0100

    5 october 23

---
+++ C0452/src/c0452/W2ex4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c0452;
+
+/**
+ *
+ * @author husna
+ */
+public class W2ex4 {
+    public static void main(String[] args) {
+        int multiplier = 7;
+        int maxMultiplicand = 12;
+        
+        for (int multiplicand = 1; multiplicand <= maxMultiplicand; multiplicand++) {
+            int result = multiplier * multiplicand;
+            System.out.println(multiplicand + " x " + multiplier + " = " + result);
+        }
+    }
+}
