bd441af61d1826ed145b1e27b403b89bde6820af
husna <husna@Sunny>
2023-10-05 20:45:13 +0100

    5 october 2023

---
+++ C0452/src/c0452/W2ex3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c0452;
+
+/**
+ *
+ * @author husna
+ */
+public class W2ex3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(i);
+        }
+    }
+}
